10024b1c597ea295fc70ca49b8a3630c79114641
Christophe Alexandre <chris.perso@gmail.com>
2016-07-01 21:59:27 +0200

    dev

--- scripts/hdbretrieve.py
+++ scripts/hdbretrieve.py
@@ -7,20 +7,23 @@ from hdb.urlcaching import set_cache_http
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(name)s:%(levelname)s:%(message)s')
     logging.getLogger('requests').setLevel(logging.WARNING)
     parser = argparse.ArgumentParser(description='Loading building data from HDB.',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter
                                      )
     parser.add_argument('output_file', type=str, nargs='?', help='name of the output CSV file', default='output.csv')
     parser.add_argument('--ntasks', type=int, help='number of simultaneous downloads', default=10)
+    parser.add_argument('--use-cache', help='stores downloaded HDB files locally', action='store_true')
     args = parser.parse_args()
     DATA_DIR = '.hdb/'
     HDB_URL = 'https://services2.hdb.gov.sg'
     set_data_dir(DATA_DIR)
     set_hdb_url(HDB_URL)
     set_pool_size(args.ntasks)
-    set_cache_http('.urlcaching')
+    if args.use_cache:
+        set_cache_http('.urlcaching')
+
     logging.info('started')
     logging.info('generating buildings data')
     generate_rooms_db()
