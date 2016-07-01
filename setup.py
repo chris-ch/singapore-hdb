eedc4e9607105e8d22a5a05369b91464265d7747
Christophe <ch.alexandre@bluewin.ch>
2016-07-01 21:47:40 +0200

    specified versions

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     author='Christophe Alexandre',
     author_email='ch.alexandre@bluewin.ch',
     description='Loading building data from HDB',
-    install_requires=['bs4', 'retrying', 'pandas', 'requests', 'requests_cache']
+    install_requires=['bs4>=4-4.4.1', 'retrying>=1.3.3', 'pandas>=0.18.1', 'requests>=2.10.0', 'requests_cache>=0.4.12']
 )
